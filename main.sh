8a4d34c93b5f5ef7285a0e6cb96cecc6854e5163
Nozdormus1 <nozdormus@mail.ru>
2017-09-18 13:20:19 +0300

    Added VPC

--- main.sh
+++ main.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # GLOBAL VARS: THESE SHOULD BE CHANGED
-VPC_ID=VPC
-SUBNETS='SUBNET-1,SUBNET-2,SUBNET-3'
 KEY_NAME=KEY_PAIR_NAME
 
 # LOCAL VARS
@@ -9,8 +7,25 @@ JNLP_PORT=8082
 REGION=eu-west-1
 AMI=ami-ebd02392
 
+
+# CREATE VPC
+VPC_ID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.VpcId' | sed 's/\"//g')
+PUBLIC_SUBNET=$(aws ec2 create-subnet --vpc-id ${VPC_ID} --cidr-block 10.0.1.0/24 --query 'Subnet.SubnetId' | sed 's/\"//g')
+PRIVATE_SUBNET=$(aws ec2 create-subnet --vpc-id ${VPC_ID} --cidr-block 10.0.0.0/24 --query 'Subnet.SubnetId' | sed 's/\"//g')
+IGWAY=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' | sed 's/\"//g')
+aws ec2 attach-internet-gateway --vpc-id ${VPC_ID} --internet-gateway-id ${IGWAY}
+PUBLIC_ROUTE_TABLE=$(aws ec2 create-route-table --vpc-id ${VPC_ID} --query 'RouteTable.RouteTableId' | sed 's/\"//g')
+aws ec2 create-route --route-table-id ${PUBLIC_ROUTE_TABLE} --destination-cidr-block 0.0.0.0/0 --gateway-id ${IGWAY}
+aws ec2 associate-route-table --subnet-id ${PUBLIC_SUBNET} --route-table-id ${PUBLIC_ROUTE_TABLE}
+NAT_EIP=$(aws ec2 allocate-address --domain vpc --query 'AllocationId' --region ${REGION} | sed 's/\"//g')
+NAT_GWAY=$(aws ec2 create-nat-gateway --subnet-id ${PUBLIC_SUBNET} --allocation-id ${NAT_EIP} --query 'NatGateway.NatGatewayId' | sed 's/\"//g')
+PRIVATE_ROUTE_TABLE=$(aws ec2 create-route-table --vpc-id ${VPC_ID} --query 'RouteTable.RouteTableId' | sed 's/\"//g')
+aws ec2 create-route --route-table-id ${PRIVATE_ROUTE_TABLE} --destination-cidr-block 0.0.0.0/0 --nat-gateway-id ${NAT_GWAY}
+aws ec2 associate-route-table --subnet-id ${PRIVATE_SUBNET} --route-table-id ${PRIVATE_ROUTE_TABLE}
+
+
+# LAUNCH JENKINS INSTANCES
 MY_IP=$(curl ipinfo.io/ip)
-MASTER_SUBNET=$(echo ${SUBNETS} | cut -d, -f1 | cut -d\' -f2)
 SLAVE_PASSWD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 sed -i "s/SLAVE_PASSWD/${SLAVE_PASSWD}/" userdata_master.sh
 sed -i "s/SLAVE_PASSWD/${SLAVE_PASSWD}/" userdata_slave.sh
@@ -23,9 +38,9 @@ aws ec2 authorize-security-group-ingress --group-id ${MASTER_SG} --protocol tcp 
 EIP_INFO=$(aws ec2 allocate-address --domain vpc --query '[PublicIp, AllocationId]' --region ${REGION} | sed 's/\"//g')
 MASTER_EIP=$(echo ${EIP_INFO} | cut -d, -f1 | cut -c 3-)
 MASTER_EIP_ID=$(echo ${EIP_INFO} | cut -d, -f2 | cut -c 2- | rev | cut -c 2- | rev)
-MASTER_INSTANCE_ID=$(aws ec2 run-instances --image-id ${AMI} --count 1 --instance-type t2.micro --key-name ${KEY_NAME} --security-group-ids ${MASTER_SG} --associate-public-ip-address --block-device-mappings file://mapping.json --user-data file://userdata_master.sh --subnet-id ${MASTER_SUBNET} --query Instances[0].InstanceId --region ${REGION} | sed 's/\"//g')
+MASTER_INSTANCE_ID=$(aws ec2 run-instances --image-id ${AMI} --count 1 --instance-type t2.micro --key-name ${KEY_NAME} --security-group-ids ${MASTER_SG} --associate-public-ip-address --block-device-mappings file://mapping.json --user-data file://userdata_master.sh --subnet-id ${PUBLIC_SUBNET} --query Instances[0].InstanceId --region ${REGION} | sed 's/\"//g')
 sleep 360
 aws ec2 associate-address --instance-id ${MASTER_INSTANCE_ID} --allocation-id ${MASTER_EIP_ID}
 sed -i "s/MASTER_EIP/${MASTER_EIP}/" userdata_slave.sh
 aws autoscaling create-launch-configuration --launch-configuration-name JenkinsSlaveLC --key-name ${KEY_NAME} --image-id ${AMI} --instance-type t2.micro --user-data file://userdata_slave.sh --security-groups ${SLAVE_SG} --associate-public-ip-address --region ${REGION}
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name JenkinsSlaveASG --launch-configuration-name JenkinsSlaveLC --min-size 1 --max-size 3 --desired-capacity 2 --vpc-zone-identifier ${SUBNETS}
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name JenkinsSlaveASG --launch-configuration-name JenkinsSlaveLC --min-size 1 --max-size 3 --desired-capacity 2 --vpc-zone-identifier ${PRIVATE_SUBNET}
